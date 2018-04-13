c563f93488b23de273f5be81e25ecea15918b4c0
Marcin Smola <Marcin_Smola@epam.com>
2018-04-13 14:40:35 +0200

    post method works

--- src/test/java/com/smola/transport/controller/TransitControllerTest.java
+++ src/test/java/com/smola/transport/controller/TransitControllerTest.java
@@ -12,14 +12,23 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.time.Month;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
 
+import java.math.BigDecimal;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -42,7 +51,9 @@ public class TransitControllerTest {
         this.mockMvc.perform(post(END_POINT)
                 .contentType(contentType)
                 .content(transitJSON))
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.sourceAddress", is("ul. Zakręt 8, Poznań")));
+
     }
 
     private Transit createSampleTransit() {
