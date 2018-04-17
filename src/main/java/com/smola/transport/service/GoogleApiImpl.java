88f9183e17d8b8221f52a0dba198c58acdcd7e9b
Marcin Smola <Marcin_Smola@epam.com>
2018-04-17 10:26:04 +0200

    google api connection works

--- src/main/java/com/smola/transport/service/GoogleApiImpl.java
+++
@@ -1,7 +0,0 @@
-package com.smola.transport.service;
-
-import com.google.maps.model.Distance;
-
-public interface GoogleApiImpl {
-    Distance calculateDistance(String sourceAddress, String destinationAddress);
-}
